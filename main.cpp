2ba458ce2475ba490c7f36c925916c3d08f300db
sebastianmusic <sebastian@jsst.no>
2025-02-12 19:51:21 +0100

    added most functions needed to start prototyping

--- main.cpp
+++ main.cpp
@@ -11,7 +11,16 @@
 #include <uv.h>
 #include <vector>
 
+//    ________    ____  ____  ___    __
+//   / ____/ /   / __ \/ __ )/   |  / /
+//  / / __/ /   / / / / __  / /| | / /
+// / /_/ / /___/ /_/ / /_/ / ___ |/ /___
+// \____/_____/\____/_____/_/  |_/_____/
+//
+
 uv_loop_t *loop = (uv_loop_t *)malloc(sizeof(uv_loop_t));
+bool isLocalMachine;
+std::vector<uv_stream_t *> connectedDaemonHandles;
 
 // hopefully unique enough tmp directory
 #define TMP_DIR "/tmp/com.sebastianmusic.nvimclipboardsync"
@@ -30,6 +39,29 @@ void createTmpDir() {
       exit(1);
     }
     std::cout << "Created temporary directory: " << TMP_DIR << std::endl;
+  }
+}
+
+void iterateOverStreams(std::vector<uv_stream_t *> vectorToIterateOver,
+                        const uv_buf_t *buf, ssize_t nread) {
+  for (uv_stream_t *handle : vectorToIterateOver) {
+
+    uv_write_t *writeRequest = new uv_write_t();
+    uv_buf_t bufs = uv_buf_init(buf->base, nread);
+
+    int result = uv_write(
+        writeRequest, handle, &bufs, 1, [](uv_write_t *req, int status) {
+          if (status < 0) {
+            std::cerr << "Error in write in daemonToDaemonReadCallback:"
+                      << uv_strerror(status) << std::endl;
+          }
+          delete req;
+        });
+
+    if (result < 0) {
+      std::cerr << "uv_write failed: " << uv_strerror(result) << std::endl;
+      delete writeRequest;
+    }
   }
 }
 
@@ -61,7 +93,7 @@ void readFromNvim(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
     // stop stream on error
     uv_read_stop(stream);
   }
-  // TODO: Implement function to pass buffer content along to connected daemon
+  iterateOverStreams(connectedDaemonHandles, buf, nread);
 }
 
 void nvimConnectCallback(uv_connect_t *req, int status) {
@@ -72,19 +104,18 @@ void nvimConnectCallback(uv_connect_t *req, int status) {
   uv_read_start(req->handle, nvimMemoryAllocationCallback, readFromNvim);
 }
 
-std::vector<std::string> connectedNvimClientsFds;
+std::vector<uv_stream_t *> connectedNvimClientsPipes;
 void tmpDirectoryUpdateEventCallback(uv_fs_event_t *handle,
                                      const char *filename, int events,
                                      int status) {
   // Might have to convert filename to type int?
   if (filename != NULL) {
-    if (std::find(connectedNvimClientsFds.begin(),
-                  connectedNvimClientsFds.end(),
-                  filename) != connectedNvimClientsFds.end()) {
+    if (std::find(connectedNvimClientsPipes.begin(),
+                  connectedNvimClientsPipes.end(),
+                  filename) != connectedNvimClientsPipes.end()) {
       std::cout << "file descriptor for nvim domain socket already exists in "
                    "vector\n";
     } else {
-      connectedNvimClientsFds.push_back(filename);
       // create a pipe type for the new unix domain socket
       uv_pipe_t newSocket;
       uv_pipe_init(loop, &newSocket, 1);
@@ -92,6 +123,7 @@ void tmpDirectoryUpdateEventCallback(uv_fs_event_t *handle,
       uv_pipe_connect(&connectionRequest, &newSocket,
                       (std::string(TMP_DIR) + filename).c_str(),
                       nvimConnectCallback);
+      connectedNvimClientsPipes.push_back((uv_stream_t *)&newSocket);
     }
   } else {
     std::cout << "filename is null\n";
@@ -114,6 +146,7 @@ void tmpDirectoryUpdateEventCallback(uv_fs_event_t *handle,
 // We have to differentiate between Localmachine and !Localmachine as
 // Localmachine will connect to all !Localmachine but !Localmachine will only
 // connect to one Localmachine
+
 static void daemonMemoryAllocationCallback(uv_handle_t *handle,
                                            size_t suggested_size,
                                            uv_buf_t *buf) {
@@ -121,20 +154,45 @@ static void daemonMemoryAllocationCallback(uv_handle_t *handle,
   buf->len = suggested_size;
 }
 
+void daemonToDaemonWriteCallback(uv_write_t *req, int status) {
+  if (status < 0) {
+    perror("error in daemonToDaemonWriteCallback");
+    return;
+  } else {
+    std::cout << "written to daemon correctly";
+  }
+}
+
+// This function sends read data to all nvim clients, if it is localMachine it
+// also sends it to every connected daemon
 void daemonToDaemonReadCallback(uv_stream_t *stream, ssize_t nread,
+
                                 const uv_buf_t *buf) {
+
   // check if nread is less than 0 if it is then there is an error
   if (nread < 0) {
     // stop stream on error
     uv_read_stop(stream);
   }
-  // TODO: Add function to pass read information to connected nvim clients
+  // TODO: Add function to pass information to connected daemon
+
+  if (isLocalMachine == true) {
+    iterateOverStreams(connectedDaemonHandles, buf, nread);
+  }
+  iterateOverStreams(connectedNvimClientsPipes, buf, nread);
 }
 
+//     __                     __                     __    _
+//    / /   ____  _________ _/ /___ ___  ____ ______/ /_  (_)___  ___
+//   / /   / __ \/ ___/ __ `/ / __ `__ \/ __ `/ ___/ __ \/ / __ \/ _ \
+//  / /___/ /_/ / /__/ /_/ / / / / / / / /_/ / /__/ / / / / / / /  __/
+// /_____/\____/\___/\__,_/_/_/ /_/ /_/\__,_/\___/_/ /_/_/_/ /_/\___/
+//
 // Connection callback for daemon to daemon communication
+//
 void daemonToDaemonConnectionCallback(uv_stream_t *server, int status) {
   if (status < 0) {
-    std::cout << "Error in Daemon to Daemon connection callback";
+    perror("Error in Daemon to Daemon connection callback");
     return;
   }
   uv_tcp_t *clientHandle = new uv_tcp_t;
@@ -142,8 +200,12 @@ void daemonToDaemonConnectionCallback(uv_stream_t *server, int status) {
   if (uv_accept(server, (uv_stream_t *)clientHandle) == 0) {
     std::cout << "Accepted new client connection\n";
 
-    uv_read_start((uv_stream_t *)clientHandle, daemonMemoryAllocationCallback,
-                  daemonToDaemonReadCallback);
+    if (uv_read_start((uv_stream_t *)clientHandle,
+                      daemonMemoryAllocationCallback,
+                      daemonToDaemonReadCallback) != 0) {
+      perror("could not start reading from client handle");
+    }
+    connectedDaemonHandles.push_back((uv_stream_t *)clientHandle);
   }
 }
 
@@ -159,7 +221,10 @@ void setupListeningSocket(int port) {
               // Set flag to 2 for SO_REUSEADDR
               (const struct sockaddr *)&listeningSocketAdresse, 2);
 
-  uv_listen(&listeningSocket, 128, daemonToDaemonConnectionCallback);
+  if (uv_listen((uv_stream_t *)&listeningSocket, 128,
+                daemonToDaemonConnectionCallback) != 0) {
+    perror("Error in uv_listen: ");
+  }
 }
 
 //  __  __    _    ___ _   _
